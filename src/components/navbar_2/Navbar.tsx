78c27eb1827738577fa7c2a86b9559526c0d52e4
YeasarArefin <yeasararefin007@gmail.com>
2024-04-21 17:54:50 +0600

    navbar-2 sidebar event propagation fixed

--- src/components/navbar_2/Navbar.tsx
+++ src/components/navbar_2/Navbar.tsx
@@ -47,7 +47,7 @@ export default function Navbar() {
                 </div>
             </div>
             <div className={cn("", { "bg-black/50 md:hidden z-30 fixed top-0 left-0 transition-all duration-300 h-[100%] w-full": mobileMenu })} onClick={() => setMobileMenu(false)}>
-                <div className={cn("flex flex-col drop-shadow-2xl w-[300px] h-[100%] md:hidden gap-y-5 pt-5 container top-0 left-0 fixed bg-white transition-all duration-200 ease-in-out", { "-left-[500px]": !mobileMenu, "-left-[0px]": mobileMenu })}>
+                <div className={cn("flex flex-col drop-shadow-2xl w-[300px] h-[100%] md:hidden gap-y-5 pt-5 container top-0 left-0 fixed bg-white transition-all duration-200 ease-in-out", { "-left-[500px]": !mobileMenu, "-left-[0px]": mobileMenu })} onClick={(e) => { e.stopPropagation(); }}>
                     <div className="flex justify-between gap-x-2 items-center w-full text-lg py-2">
                         <div className="flex items-center gap-x-3">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="h-9 w-9"><rect width="256" height="256" fill="none"></rect><line x1="208" y1="128" x2="128" y2="208" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></line><line x1="192" y1="40" x2="40" y2="192" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="16"></line></svg>
